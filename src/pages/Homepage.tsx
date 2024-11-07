ef625095c61e0aae107db090e77776045c1f5980
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-07 09:14:53 +0100

    create Container component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,18 +1,9 @@
 import Button from "../components/button/Button";
+import Container from "../components/container/Container";
 import Typography from "../components/typography/Typography";
 
 const Homepage = () => {
-  return (
-    <div className="mx-4">
-      Homepage
-      <div>
-        <Button text="get ticket now" variant="primary" size="large" />
-        {/* <Button text="get ticket now" variant="outline" size="large" /> */}
-        <Typography variant="h1">The Grand Gala</Typography>
-        <Typography variant="h2">What to expect at the event</Typography>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default Homepage;
