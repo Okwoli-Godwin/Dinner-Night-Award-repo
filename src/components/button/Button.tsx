25695c4fdb63952cb98c684b879629d75dbe96e4
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-07 08:19:23 +0100

    create Button component

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./button.module.css";
+
+type ButtonProps = {
+  text: string;
+  variant?: "primary" | "secondary" | "outline";
+  size?: "small" | "medium" | "large";
+};
+
+const Button: React.FC<ButtonProps> = ({
+  text,
+  variant = "primary",
+  size = "medium",
+}) => {
+  return (
+    <button className={`${styles.button} ${styles[variant]} ${styles[size]}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,15 @@
-
+import Button from "../components/button/Button";
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="mx-4">
       Homepage
+      <div>
+        <Button text="get ticket now" variant="primary" size="large" />
+        <Button text="get ticket now" variant="outline" size="large" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
