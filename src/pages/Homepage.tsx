fb092d16e9302e53a6f0a083cef8913823d28192
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-07 08:58:39 +0100

    create typography component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import Button from "../components/button/Button";
+import Typography from "../components/typography/Typography";
 
 const Homepage = () => {
   return (
@@ -6,7 +7,9 @@ const Homepage = () => {
       Homepage
       <div>
         <Button text="get ticket now" variant="primary" size="large" />
-        <Button text="get ticket now" variant="outline" size="large" />
+        {/* <Button text="get ticket now" variant="outline" size="large" /> */}
+        <Typography variant="h1">The Grand Gala</Typography>
+        <Typography variant="h2">What to expect at the event</Typography>
       </div>
     </div>
   );
