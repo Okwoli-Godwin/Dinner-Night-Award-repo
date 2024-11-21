ca8a51ce0a7dd9b4403d549d4e31ee06cd598dd4
Babatunde-Segun <officiallyvincify@gmail.com>
2024-11-21 14:24:51 +0100

    implement eventWeekSection

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,6 @@
 import CountDownToEvent from "../components/homePage-components/countdown-to-event/CountDownToEvent";
 import EventCallToAction from "../components/homePage-components/event-call-action/EventCallToAction";
+import EventWeekSection from "../components/homePage-components/event-week/EventWeekSection";
 import Hero from "../components/homePage-components/hero-section/Hero";
 import IntroSection from "../components/homePage-components/introduction/Introduction";
 // import ShopWithSponsors from "../components/homePage-components/shop-with-sponsor/ShopWithSponsor";
@@ -15,6 +16,7 @@ const Homepage = () => {
       <ShopWithSponsors />
       <EventCallToAction />
       <CountDownToEvent />
+      <EventWeekSection />
     </>
   );
 };
